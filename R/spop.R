25ddef31999437718c8aa52135d7d4e032994707
Kamil Erguler <kerguler@gmail.com>
2018-11-21 17:37:59 +0200

    bug fix
    perturbate no longer iterates devcycle

--- R/spop.R
+++ R/spop.R
@@ -191,7 +191,7 @@ iterfun <- function(x, value, pause) {
     }
     x@devtable <- x@pop
     x@devtable$number <- d 
-    x@devtable$devcycle <- x@devtable$devcycle + 1
+    x@devtable$devcycle <- if (pause) {x@devtable$devcycle} else {x@devtable$devcycle + 1}
     x@devtable$development <- 0
     x@devtable <- x@devtable[x@devtable$number>0,]
     
